e3c3967f6c0023196f8dfedaf2ce498cf6eedb84
lenovo <stanimir_cena@abv.bg>
2024-08-13 11:39:46 +0300

    Add project files.

---
+++ Cars/ICar.cs
@@ -0,0 +1,12 @@
+using System.Security.Principal;
+
+namespace Cars;
+
+public interface ICar
+{
+    public string Model { get; set; }
+    public string Color { get; set; }
+
+    public string Start();
+    public string Stop();
+}
